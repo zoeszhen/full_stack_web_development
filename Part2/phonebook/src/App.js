04ae2438306fae896e6d794a5755b3ae038e87b8
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-09-08 20:49:45 +0300

    - add new name function - render name list

---
+++ Part2/phonebook/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit" onClick={(e) => {
+            e.preventDefault()
+            setPersons((prevState) => [...prevState, { name: newName }])
+            setNewName("")
+          }}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {
+          persons.map(({ name }) => <div>{name}</div>)
+        }
+      </div>
+    </div>
+  )
+
+}
+
+export default App
