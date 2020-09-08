b491288b66ddfc849a3cc6824da24a5971934a37
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-09-08 20:56:19 +0300

    add new number

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1231245' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   return (
     <div>
@@ -14,12 +15,15 @@ const App = () => {
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
+        </div>
+        <div>
           <button type="submit" onClick={(e) => {
             e.preventDefault()
             if (persons.map(({ name }) => name).includes(newName)) {
               window.alert(`${newName} is already added to phonebook`)
             } else {
-              setPersons((prevState) => [...prevState, { name: newName }])
+              setPersons((prevState) => [...prevState, { name: newName, number: newNumber }])
             }
             setNewName("")
           }}>add</button>
