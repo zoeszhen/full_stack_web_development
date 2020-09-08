eee5e11297636fd1f8ac93f6c60dcc7c2f3c8a60
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-09-08 21:33:22 +0300

    get data from api

--- Part2/phonebook/src/Persons.js
+++ Part2/phonebook/src/Persons.js
@@ -1,8 +1,7 @@
 import React from 'react'
 export const Persons = ({ persons, filter }) => <div>
     {
-        persons
-            .filter(({ name }) => name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+        persons && persons.filter(({ name }) => name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
             .map(({ name }) => <div key={name}>{name}</div>)
     }
 </div>
