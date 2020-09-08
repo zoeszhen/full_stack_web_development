6d1e88475d3460071a8360e161d759fac5ef226e
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-09-08 21:01:03 +0300

    add search function

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1231245' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>PhoneBook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
@@ -32,7 +41,9 @@ const App = () => {
       <h2>Numbers</h2>
       <div>
         {
-          persons.map(({ name }) => <div>{name}</div>)
+          persons
+            .filter(({ name }) => name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+            .map(({ name }) => <div>{name}</div>)
         }
       </div>
     </div >
