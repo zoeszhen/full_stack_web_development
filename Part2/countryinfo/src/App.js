9f3a210a878651f06e6222ffd20adbfb669dc5d3
SHI ZHEN <zoe.zhenshi@gmail.com>
2020-09-09 20:12:53 +0300

    - search country - show details - if under 10 and over 1 show button

--- Part2/countryinfo/src/App.js
+++ Part2/countryinfo/src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
+const API = 'https://restcountries.eu/rest/v2/all'
 function App() {
+  //States
+  const [countries, setCountries] = useState([]);
+  const [searchCountry, setSearchCountry] = useState('');
+  const [filteredContries, setFilteredContries] = useState([]);
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        await axios.get(API).then((response) => {
+          setCountries(response.data)
+        });
+      } catch (error) {
+        throw error;
+      }
+    };
+    fetchCountries();
+  }, []);
+
+  //Event handlers
+  const handleSearchChange = (value) => {
+    setSearchCountry(value);
+    setFilteredContries(
+      countries.filter((e) =>
+        e.name.toLowerCase().includes(value.toLowerCase())
+      )
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        Find countries: <input value={searchCountry} onChange={(e) => handleSearchChange(e.target.value)} />
+      </div>
+      {
+        filteredContries.length > 10 && <div> Too many matches, specify another filter</div>
+      }
+      {
+        filteredContries.length < 10 &&
+        filteredContries.length > 1 &&
+        filteredContries.map((conuntry) =>
+          <div key={conuntry.name}>
+            {conuntry.name}
+            <button onClick={() => { setFilteredContries([conuntry]) }}>show</button>
+          </div>)
+      }
+      {
+        filteredContries.length === 1 &&
+        filteredContries.map(({ name, capital, population, languages, flag }) =>
+          <div>
+            <h1>{name}</h1>
+            <div>capital {capital}</div>
+            <div>population {population}</div>
+            <h3>languages</h3>
+            <ul>
+              {languages.map((e) => (
+                <li key={e.name}>{e.name}</li>
+              ))}
+            </ul>
+            <img
+              src={flag}
+              alt={`flag of ${name}`}
+              style={{ maxWidth: '20%' }}
+            />
+          </div>)
+      }
     </div>
   );
 }
